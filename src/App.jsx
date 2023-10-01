7b4bc02973a60252b58321c3715e4becba12bc68
alexander duque <alexduque541@gmail.com>
2023-10-01 16:45:19 +0200

    prueba2

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { useState } from "react"
-import React, { useEffect } from 'react';
 import Formulario from "./components/Formulario"
 import Header from "./components/Header"
 import ListadoPacientes from "./components/ListadoPacientes"
@@ -9,71 +8,46 @@ import "./index.css"
 
 function App() {
 
-  useEffect(() => {
-    const iframe = document.getElementById('video-iframe');
+  const [pacientes, setPacientes] = useState([]);
 
-    // Función para reproducir el video cuando la página se carga
-    const playVideo = () => {
-      if (iframe) {
-        iframe.src += '&autoplay=1'; // Agrega el parámetro 'autoplay=1' a la URL del iframe para iniciar la reproducción automática
-      }
-    };
+  const [paciente, setPaciente] = useState({}); //FUNCION
 
-    // Escucha el evento 'load' del iframe y llama a la función para reproducir el video
-    if (iframe) {
-      iframe.addEventListener('load', playVideo);
-    }
+  const eliminarPaciente = (id) => {
 
-    return () => {
-      // Limpia el evento cuando el componente se desmonta
-      if (iframe) {
-        iframe.removeEventListener('load', playVideo);
-      }
-    };
-  }, []);
-  
-const [pacientes, setPacientes] =useState([]);
-
-const[paciente, setPaciente] = useState({}); //FUNCION
-
-const eliminarPaciente = (id) => {
-
-  const pacientesActualizados = pacientes.filter(paciente => paciente.id !== id)
-  setPacientes(pacientesActualizados)
-}
+    const pacientesActualizados = pacientes.filter(paciente => paciente.id !== id)
+    setPacientes(pacientesActualizados)
+  }
 
 
 
   return (
-<div className="video-background">
-      <iframe
-        id="video-iframe"
-        title="Video Embebido"
-        src="https://mega.nz/embed/pmUxkaBA#t9N_qn0YCeETOb1XOkhbPWVpPpRxTCc4ea8dvH_Rrww!1a1m" 
-        allowfullscreen allow="autoplay;"
-        frameborder="0"
-      ></iframe>
-      {/* Contenido adicional */}
+    <div className="video-background">
+      <video autoPlay muted loop id="myVideo">
+        <source src="src/assets/video-body.mp4" type="video/mp4" />
+        Tu navegador no admite el elemento de video.
+      </video>
+      <div className="content">
+        {/* Contenido de tu página */}
+      </div>
+      <Header
 
-<Header
+      /> {/*EN JSX SE CIERRA ASI LAS ETIQUETAS SIN CIERRE /> */}
 
-/> {/*EN JSX SE CIERRA ASI LAS ETIQUETAS SIN CIERRE /> */}
+      <div className="mt-10 md:flex">
+        <Formulario
+          pacientes={pacientes}
+          setPacientes={setPacientes}
+          paciente={paciente}
+          setPaciente={setPaciente}
+        />
+        <ListadoPacientes
+          pacientes={pacientes}
+          setPaciente={setPaciente}
+          eliminarPaciente={eliminarPaciente}
+        />
+      </div>
 
-<div className="mt-10 md:flex">
-<Formulario
-  pacientes= {pacientes}
-  setPacientes= {setPacientes}
-  paciente = {paciente}
-  setPaciente = {setPaciente}
-/>
-<ListadoPacientes
-  pacientes= {pacientes}
-  setPaciente= {setPaciente}
-  eliminarPaciente = {eliminarPaciente}
-/>
-</div>
-
-</div>
+    </div>
   )
 }
 
